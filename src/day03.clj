cca0c864ba684cb8056ed816fa0c127cabc30483
Tim Beck <socaltdb@gmail.com>
2023-01-23 21:34:28 -0800

    Day 3

--- src/day03.clj
+++ src/day03.clj
@@ -1,9 +1,12 @@
-(ns day03)
+(ns day03
+  (:require [clojure.set :as set]))
 
 (defn parse-line
-  "Parse single line of input"
+  "transform single line of input"
   [line]
-  line)
+  (split-at 
+    (quot (count line) 2) 
+    line))
 
 (defn read-stdin
   "Read all lines from stdin"
@@ -15,10 +18,29 @@
        (read-line)
        (conj acc (parse-line input))))))
 
+(defn common-item
+  [compartments]
+  (first (set/intersection 
+    (set (first compartments)) 
+    (set (second compartments)))))
+
+(defn priority-value
+  [item]
+  (if (>= (int item) 97)
+    (- (int item) 96)
+    (- (int item) 38)))
+
 (defn solve
-  [rucksack]
-  rucksack)
+  [rucksacks]
+  (if (= rucksacks [])
+    0
+    (+
+      (priority-value (common-item (peek rucksacks)))
+      (solve (pop rucksacks)))))
 
 (defn run [opts]
-  (def all-games (read-stdin))
-  (println (solve (read-stdin)))
+  (def rucksacks (read-stdin))
+  (println (int \a) (int \A))
+  (println (map common-item rucksacks))
+  (println (map priority-value (map common-item rucksacks)))
+  (println (solve rucksacks)))
